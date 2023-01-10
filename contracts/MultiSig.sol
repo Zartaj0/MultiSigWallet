e2b3e33609350be1a8f8e0fd136d1620e4b8f69d
zartaj <zartaj.afsar@gmail.com>
2023-01-10 22:16:32 +0400

    added contract

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -1,34 +1,114 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract Multisig {
+    address[] owners;
+    Transaction[] transactions;
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+    mapping(address => bool) private isOwner;
+    mapping(uint256 => mapping(address => bool)) private confirmed;
 
-    event Withdrawal(uint amount, uint when);
+    uint256 required;
+    struct Transaction {
+        address to;
+        uint256 amount;
+        bytes data;
+        uint256 txIndex;
+        bool executed;
+        uint256 confirmCount;
+    }
 
-    constructor(uint _unlockTime) payable {
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "you are not an ownerFF");
+        _;
+    }
+
+    modifier txExist(uint256 _txIndex) {
+        require(_txIndex < transactions.length, " transaction doesn't exist");
+        _;
+    }
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "Already executed");
+        _;
+    }
+    modifier notConfirmed(uint256 _txIndex) {
+        require(!confirmed[_txIndex][msg.sender], "Already confirmed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint256 _required) {
+        require(_owners.length > 1, "must be mmore than 1 owner");
         require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+            _owners.length >= _required && _required > 0,
+            "Invalid require input"
         );
+        for (uint256 i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+            require(owner != address(0), "invalid address");
+            require(!isOwner[owner], "Owner is already added");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        required = _required;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function submit(
+        address _to,
+        uint256 _amount,
+        bytes calldata _data
+    ) external onlyOwner {
+        uint256 _txIndex = transactions.length;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        confirmed[_txIndex][msg.sender]= true;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+        transactions.push(
+            Transaction({
+                to: _to,
+                amount: _amount,
+                data: _data,
+                txIndex: _txIndex,
+                executed: false,
+                confirmCount: 1
+            })
+        );
     }
+
+    function approve(uint256 _txIndex)
+        external
+        onlyOwner
+        txExist(_txIndex)
+        notConfirmed(_txIndex)
+        notExecuted(_txIndex)
+    {
+        confirmed[_txIndex][msg.sender] = true;
+        transactions[_txIndex].confirmCount += 1;
+        if (transactions[_txIndex].confirmCount == required) {
+            execute(_txIndex);
+        }
+    }
+
+    function execute(uint256 _txIndex) internal notExecuted(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+
+        transaction.executed = true;
+        (bool result, ) = transaction.to.call{value: transaction.amount}("");
+
+        require(result, " tx failed ");
+    }
+
+    function revoke() external {}
+
+    function allTxs() external view returns(Transaction[] memory){
+        return transactions;
+    }
+
+    function singleTx(uint _index) view external returns(Transaction memory) {
+        return transactions[_index];
+
+    }
+
+    receive() external payable {}
 }
