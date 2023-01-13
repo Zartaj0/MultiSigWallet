3771cf411334e58493642dd26e04fc9da722934f
zartaj <zartaj.afsar@gmail.com>
2023-01-13 15:36:01 +0400

    added dependencies for verification of contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,5 +15,10 @@ module.exports = {
       accounts:[process.env.PRIVATE_KEY1],
       url:process.env.ALCHEMY_MUMBAI_URL
     }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN
   }
 };
