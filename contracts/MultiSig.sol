5b693cf62d870d8eb1b5f58984d5a438acc9491b
zartaj <zartaj.afsar@gmail.com>
2023-01-15 01:16:42 +0400

    adding some dynamic fuinctionality

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -11,42 +11,56 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MultiSig {
     //events
-    event DepositedEther(address depositor, uint amount, uint timestamp);
+    event DepositedEther(address depositor, uint256 amount, uint256 timestamp);
     event DepositedErc20(
         address depositor,
-        uint amount,
+        uint256 amount,
         address TokenAddress,
-        uint timestamp
+        uint256 timestamp
     );
     event SubmittedErc20(
         address SubmittedBy,
         address to,
         address TokenAddress,
-        uint amount,
-        uint timestamp
+        uint256 amount,
+        uint256 timestamp
     );
     event SubmittedEther(
         address SubmittedBy,
         address to,
-        uint amount,
-        uint timestamp
+        uint256 amount,
+        uint256 timestamp
     );
-    event Approved(address approvedBy, Transaction transaction, uint timestamp);
-    event Executed(address to, Transaction transaction, uint timestamp);
+    event Approved(
+        address approvedBy,
+        Transaction transaction,
+        uint256 timestamp
+    );
+    event Executed(address to, Transaction transaction, uint256 timestamp);
 
     //state Variables
     uint256 requiredApproval;
     address[] owners;
     Transaction[] transactions;
+    Proposal[] proposals;
+
     //mapppings
     mapping(address => bool) private isOwner;
-    mapping(uint256 => mapping(address => bool)) private confirmed;
-    mapping(uint => address) internal TokenAddress;
+    mapping(uint256 => mapping(address => bool)) private confirmedTx;
+    mapping(uint256 => mapping(address => bool)) private confirmedProposal;
+    mapping(uint256 => OwnerProposal) internal OwnerMap;
+    mapping(uint256 => ChangeRequiredSign) internal PolicyMap;
+    mapping(uint256 => address) internal TokenAddress;
 
     //enum
     enum Type {
         ERC20,
         Ether
+    }
+    enum ProposalType {
+        RevokeOwner,
+        AddNewOwner,
+        ChangeRequired
     }
 
     //structs
@@ -60,6 +74,107 @@ contract MultiSig {
         uint256 confirmCount;
         Type _type;
     }
+
+    struct Proposal {
+        address submittedBy;
+        ProposalType proposalType;
+        uint256 Index;
+        bool executed;
+        uint256 confirmCount;
+    }
+
+    struct OwnerProposal {
+        address owner;
+        ProposalType proposalType;
+    }
+
+    struct ChangeRequiredSign {
+        uint256 previousSignRequirement;
+        uint256 newRequiredSign;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    function submitProposalForOwner(
+        uint8 _proposalType,
+        address _owner,
+        uint256 _requiredSign
+    ) external onlyOwner {
+        if (_proposalType == 0) {
+            uint256 _Index = proposals.length;
+            proposals.push(
+                Proposal({
+                    submittedBy: msg.sender,
+                    proposalType: ProposalType(_proposalType),
+                    Index: _Index,
+                    executed: false,
+                    confirmCount: 1
+                })
+            );
+            OwnerMap[_Index] = OwnerProposal({
+                owner: _owner,
+                proposalType: ProposalType(_proposalType)
+            });
+        } else if (_proposalType == 1) {
+            uint256 _Index = proposals.length;
+            proposals.push(
+                Proposal({
+                    submittedBy: msg.sender,
+                    proposalType: ProposalType(_proposalType),
+                    Index: _Index,
+                    executed: false,
+                    confirmCount: 1
+                })
+            );
+            OwnerMap[_Index] = OwnerProposal({
+                owner: _owner,
+                proposalType: ProposalType(_proposalType)
+            });
+        } else if (_proposalType == 2) {
+            uint256 _Index = proposals.length;
+            proposals.push(
+                Proposal({
+                    submittedBy: msg.sender,
+                    proposalType: ProposalType(2),
+                    Index: _Index,
+                    executed: false,
+                    confirmCount: 1
+                })
+            );
+            PolicyMap[_Index] = ChangeRequiredSign({
+                previousSignRequirement: requiredApproval,
+                newRequiredSign: _requiredSign
+            });
+        }
+    }
+
+    function approveProposal(uint256 _index) external onlyOwner {
+        require(_index < proposals.length, "invalid index");
+        require(!confirmedProposal[_index][msg.sender], "Already approved");
+
+        confirmedProposal[_index][msg.sender] = true;
+        proposals[_index].confirmCount += 1;
+    }
+
+    function executeProposal(uint256 _index) external onlyOwner {
+        require(!proposals[_index].executed, "Already executed");
+        require(
+            proposals[_index].confirmCount == requiredApproval,
+            "Not approved by everyone"
+        );
+
+        if (proposals[_index].proposalType == ProposalType(0)) {
+            address ownerToRemove = OwnerMap[_index].owner;
+            isOwner[ownerToRemove] = false;
+            for (uint256 i; i < proposals.length; ++i) {
+                proposals[i] = proposals[i + 1];
+            }
+            proposals.pop();
+        } else if (
+            proposals[_index].proposalType == ProposalType(1)
+        ) {} else if (proposals[_index].proposalType == ProposalType(2)) {}
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
 
     //modifiers
     modifier onlyOwner() {
@@ -75,8 +190,8 @@ contract MultiSig {
         require(!transactions[_txIndex].executed, "Already executed");
         _;
     }
-    modifier notConfirmed(uint256 _txIndex) {
-        require(!confirmed[_txIndex][msg.sender], "Already confirmed");
+    modifier notconfirmedTx(uint256 _txIndex) {
+        require(!confirmedTx[_txIndex][msg.sender], "Already confirmedTx");
         _;
     }
 
@@ -84,7 +199,7 @@ contract MultiSig {
     constructor(address[] memory _owners, uint256 _required) {
         require(_owners.length > 1, "must be mmore than 1 owner");
         require(
-            _owners.length >= _required && _required > 0,
+            _owners.length >= _required && _required > 1,
             "Invalid require input"
         );
         for (uint256 i = 0; i < _owners.length; i++) {
@@ -109,29 +224,39 @@ contract MultiSig {
         return transactions;
     }
 
-    function singleTx(
-        uint _index
-    ) external view returns (Transaction memory transaction) {
+    function singleTx(uint256 _index)
+        external
+        view
+        returns (Transaction memory transaction)
+    {
         return (transactions[_index]);
     }
 
-    function balanceErc20(address ERC20) public view returns (uint) {
+    function balanceErc20(address ERC20) public view returns (uint256) {
         return IERC20(ERC20).balanceOf(address(this));
+    }
+
+    function TokenAddressForsubmittedTx(uint256 _txIndex)
+        external
+        view
+        returns (address)
+    {
+        return TokenAddress[_txIndex];
     }
 
     //write functions
     function submitERC20Tx(
         address _to,
         address ERC20,
-        uint _amount,
+        uint256 _amount,
         bytes memory _data
     ) external onlyOwner {
-        uint balance = balanceErc20(ERC20);
+        uint256 balance = balanceErc20(ERC20);
 
         require(_amount <= balance, "Not enough balance");
         uint256 _txIndex = transactions.length;
 
-        confirmed[_txIndex][msg.sender] = true;
+        confirmedTx[_txIndex][msg.sender] = true;
 
         transactions.push(
             Transaction({
@@ -158,7 +283,7 @@ contract MultiSig {
     ) external onlyOwner {
         uint256 _txIndex = transactions.length;
 
-        confirmed[_txIndex][msg.sender] = true;
+        confirmedTx[_txIndex][msg.sender] = true;
 
         transactions.push(
             Transaction({
@@ -176,19 +301,24 @@ contract MultiSig {
         emit SubmittedEther(msg.sender, _to, _amount, block.timestamp);
     }
 
-    function approveTx(
-        uint256 _txIndex
-    )
+    function approveTx(uint256 _txIndex)
         external
         onlyOwner
         txExist(_txIndex)
         notExecuted(_txIndex)
-        notConfirmed(_txIndex)
+        notconfirmedTx(_txIndex)
     {
-        confirmed[_txIndex][msg.sender] = true;
-        transactions[_txIndex].confirmCount += 1;
-        if (transactions[_txIndex].confirmCount == requiredApproval) {
-            executeTx(_txIndex);
+        Transaction storage transaction = transactions[_txIndex];
+
+        confirmedTx[_txIndex][msg.sender] = true;
+        transaction.confirmCount += 1;
+        if (transaction.confirmCount == requiredApproval) {
+            if (
+                transaction._type == Type.ERC20 ||
+                transaction._type == Type.Ether
+            ) {
+                executeTx(_txIndex);
+            }
         }
         emit Approved(msg.sender, transactions[_txIndex], block.timestamp);
     }
