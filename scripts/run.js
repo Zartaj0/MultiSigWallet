8a920f8a2619dc70af1222e9de777e368c2e198a
zartaj <zartaj.afsar@gmail.com>
2023-01-16 11:21:48 +0400

    script for creating wallet

---
+++ scripts/run.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const send_abi = require("./abi.json");
+
+let contract_address = "0x83b3c46a13512E692e656F9FCDa81667F72BB95E"
+
+ethersProvider = new ethers.providers.AlchemyProvider("maticmum",process.env.ALCHEMY_MUMBAI_API)
+
+ 
+let arrayOfPrivateKeys = [
+    process.env.PRIVATE_KEY3, 
+    
+]
+let arrayOfOwners =["0x69A0d70271fb5C402a73125D95fadA17C55aD89A","0x1af9C19A1513B9D05a7E5CaAd9F9239EF54fE2b1","0xD6E5C56b74841d333938860F7949faa8F991d88D"];
+
+
+async function createWallet(
+    contract_address,
+    _address,
+    private_key,
+
+) {
+    for (const i of private_key) {
+
+        let wallet = await new ethers.Wallet(i);
+        let walletSigner = await wallet.connect(ethersProvider);
+
+            if (contract_address) {
+              
+                let contract = await new ethers.Contract(
+                    contract_address,
+                    send_abi,
+                    walletSigner
+                )
+
+            
+                
+                  await contract.CreateWallet(_address,3).then((transferResult) => {
+                        console.dir(transferResult)
+                        console.log("sent token")
+                    }).catch(error => console.log(error))
+                
+            }
+    }
+
+}
+
+createWallet(
+    contract_address,
+    arrayOfOwners,
+    
+    arrayOfPrivateKeys
+)
