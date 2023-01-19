34519e80e4efbb6e45760cf17ff13b122432a78d
zartaj <zartaj.afsar@gmail.com>
2023-01-19 12:33:22 +0400

    updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.16",
   settings: {
     optimizer: {
       enabled: true,
