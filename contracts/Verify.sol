b0cba906ab692892dac19ed0d76d01a17b6c28a2
zartaj <zartaj.afsar@gmail.com>
2023-06-17 22:52:23 +0530

    verification updated

--- contracts/Verify.sol
+++ contracts/Verify.sol
@@ -1,31 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 contract VerifySignature {
+
     function getMessageHash(
-        string memory _message
+        address _to,
+        uint _amount,
+        string memory _message,
+        uint _nonce
     ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_message));
+        return keccak256(abi.encodePacked(_to, _amount, _message, _nonce));
     }
 
+ 
     function getEthSignedMessageHash(
         bytes32 _messageHash
     ) public pure returns (bytes32) {
+        /*
+        Signature is produced by signing a keccak256 hash with the following format:
+        "\x19Ethereum Signed Message\n" + len(msg) + msg
+        */
         return
             keccak256(
-                abi.encodePacked(
-                    "\x19Ethereum Signed Message:\n32",
-                    _messageHash
-                )
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
             );
     }
 
     function verify(
         address _signer,
+        address _to,
+        uint _amount,
         string memory _message,
+        uint _nonce,
         bytes memory signature
     ) public pure returns (bool) {
-        bytes32 messageHash = getMessageHash(_message);
+        bytes32 messageHash = getMessageHash(_to, _amount, _message, _nonce);
         bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
 
         return recoverSigner(ethSignedMessageHash, signature) == _signer;
@@ -36,8 +46,8 @@ contract VerifySignature {
         bytes memory _signature
     ) public pure returns (address) {
         (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
-        address signerRecoverd = ecrecover(_ethSignedMessageHash, v, r, s);
-        return signerRecoverd;
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
     }
 
     function splitSignature(
