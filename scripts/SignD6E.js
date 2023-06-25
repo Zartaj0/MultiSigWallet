9b1267e3c5229f94afee9bd5ce589766ce5107be
zartaj <zartaj.afsar@gmail.com>
2023-06-25 11:32:47 +0530

    Signing Script added

---
+++ scripts/SignD6E.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+// let messageHash;
+
+const SignMessage = async () => {
+    const VerifySignature = await ethers.getContractFactory("VerifySignature")
+    const verify = await VerifySignature.deploy()
+    await verify.deployed()
+    const PRIV_KEY = process.env.PRIVATE_KEY3;
+    const signer = new ethers.Wallet(PRIV_KEY)
+    const hash = await verify.getMessageHash(0, 0)
+    const sig = await signer.signMessage(ethers.utils.arrayify(hash))
+    console.log(sig);
+    console.log("WRONG SIGNATURE");
+    const hash2 = await verify.getMessageHash(0, 1)
+    const sig2 = await signer.signMessage(ethers.utils.arrayify(hash2))
+    console.log(sig2);
+};
+SignMessage();
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,7 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const array = ["0x69A0d70271fb5C402a73125D95fadA17C55aD89A","0x1af9C19A1513B9D05a7E5CaAd9F9239EF54fE2b1","0xD6E5C56b74841d333938860F7949faa8F991d88D"];
 
 async function main() {
 
@@ -13,8 +14,12 @@ async function main() {
   // const multisig = await MultiSig.deploy();
 
   // await multisig.deployed();
-
-  const Factory = await hre.ethers.getContractFactory("Factory");
+  const library = await ethers.getContractFactory("VerifySignature");
+  lib = await library.deploy();
+  
+  const Factory = await hre.ethers.getContractFactory("Factory", {
+    libraries:{ VerifySignature: lib.address }
+  });
   const factory = await Factory.deploy();
 
   await factory.deployed();
@@ -22,13 +27,27 @@ async function main() {
  
 
   console.log(`Factory deployed ${factory.address}`);
-  await factory.deployTransaction.wait(5);
+  console.log(`Deploying MultiSig wallet`);
+  const tx = await factory.createWallet(array,3);
+  const rc = await tx.wait(); // 0ms, as tx is already confirmed
+  const event = rc.events.find(event => event.event === 'Created');
+  const [multisig, to] = event.args;
+
+
+  await factory.deployTransaction.wait(10);
 
   await hre.run("verify:verify", {
     address: factory.address,
     contract: "contracts/Factory.sol:Factory", //Filename.sol:ClassName
     constructorArguments: [],
  });
+
+ await hre.run("verify:verify", {
+  address: multisig,
+  contract: "contracts/MultiSig.sol:MultiSig", //Filename.sol:ClassName
+  constructorArguments: [array,3],
+});
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
